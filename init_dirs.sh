cab251291644b68083adf0b2876ad6e2a67d5f20
Jakob Elias <jelias1@luc.edu>
2022-02-02 12:57:04 -0600

    initial commit

---
+++ init_dirs.sh
@@ -0,0 +1,9 @@
+mkdir kafka
+mkdir kafka/data
+mkdir zookeeper
+mkdir zookeeper/data
+mkdir zookeeper/log
+mkdir postgres
+
+sudo chown -R 1000:1000 kafka
+sudo chown -R 1000:1000 zookeeper
---
+++ set_env.sh
@@ -0,0 +1,18 @@
+# CHANGE these lines as necessary
+export MDML_HOST="merfpoc.egs.anl.gov"
+export MDML_SCHEMA_HOST="merfpoc.egs.anl.gov"
+
+export KAFKA_DATA_PATH="/home/jelias/anl-mdml/mdml-minimal/kafka/data"
+
+export ZOOKEEPER_DATA_PATH="/home/jelias/anl-mdml/mdml-minimal/zookeeper/data"
+export ZOOKEEPER_LOG_PATH="/home/jelias/anl-mdml/mdml-minimal/zookeeper/log"
+
+export POSTGRES_DATA_PATH="/home/jelias/anl-mdml/mdml-minimal/postgres"
+
+export MDML_GRAFANA_SECRET="password"
+export MDML_GRAFDB_SECRET="password"
+export MDML_GRAFDB_ROOT_SECRET="password"
+export POSTGRES_PASSWORD="password"
+
+sudo rm $KAFKA_DATA_PATH/meta.properties
+
